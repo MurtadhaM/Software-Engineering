299996901a91ee8073bd199853385401b1e8afdb
Murtadha Marzouq <mmarzou0@gmail.com>
2021-11-09 09:43:16 -0500

    Making the login page the first page

--- Final Project/Travel_App/Flask/Travel_app.py
+++ Final Project/Travel_App/Flask/Travel_app.py
@@ -32,7 +32,7 @@ with app.app_context():
 # Setting up the routes
 @app.route('/')
 def home():
-    return render_template("Home.html")
+    return render_template("login.html")
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
